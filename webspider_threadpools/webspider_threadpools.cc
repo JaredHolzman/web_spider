d849dbff0b93c603f907207aa4ad0a5a1e6a0a80
Jared Holzman <jared@holzman.io>
2017-03-21 13:35:06 -0400

    Tracking aprox depth

--- webspider_threadpools/webspider_threadpools.cc
+++ webspider_threadpools/webspider_threadpools.cc
@@ -19,6 +19,7 @@ void CrawlWebPools(std::string *root_webpage_address, int max_threads,
   void *status;
 
   tsqueue->append(root_webpage_address);
+  tsqueue->append(NULL);
 
   // Create threads to crawl webpages
   for (long i = 0; i < max_threads; i++) {
@@ -32,30 +33,44 @@ void CrawlWebPools(std::string *root_webpage_address, int max_threads,
       printf("ERROR; return code from pthread_join() is %d\n", rc);
       exit(-1);
     }
-    printf("Producer: completed join with thread %d having a status of %ld\n",
+    printf("Spider: completed join with thread %d having a status of %ld\n",
            i, (long)status);
   }
 }
 
 void *crawl_page(void *threadID) {
   while (max_depth > 0) {
-    pthread_mutex_lock(&lock);
     // Exit if max depth has been reached
-    if (*max_depth == 0) {
+    pthread_mutex_lock(&lock);
+    if (*max_depth <= 0) {
       pthread_mutex_unlock(&lock);
       break;
     }
-
-    *max_depth -= 1;
+    // *max_depth -= 1;
     pthread_mutex_unlock(&lock);
 
     std::string *webpage_address = tsqueue->remove();
+
+    // Decremenent depth every time a depth marker is hit
+    while (webpage_address == NULL) {
+      pthread_mutex_lock(&lock);
+      // if (*max_depth == 0) {
+      //   pthread_mutex_unlock(&lock);
+      //   pthread_exit(threadID);
+      // }
+      *max_depth -= 1;
+      // std::cout << *max_depth << std::endl;
+      pthread_mutex_unlock(&lock);
+      webpage_address = tsqueue->remove();
+    }
+
     std::vector<std::string *> linked_pages =
         scraper->get_page_hrefs(*webpage_address);
     for (size_t i = 0; i < linked_pages.size(); i++) {
       std::cout << *linked_pages[i] << std::endl;
       tsqueue->append(linked_pages[i]);
     }
+    tsqueue->append(NULL);
   }
   pthread_exit(threadID);
 }
