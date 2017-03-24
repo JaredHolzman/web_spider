5eee2e2eeb6d92534a64054eca09fd9bfd9f66e6
Jared Holzman <jared@holzman.io>
2017-03-23 23:20:48 -0400

    Parse urls correctly

--- webpage_scraper/webpage_scraper.cc
+++ webpage_scraper/webpage_scraper.cc
@@ -77,14 +77,14 @@ WebPageScraper::parse_html(std::string page_html, std::string webpage_address) {
     }
 
     GumboAttribute *href;
+    std::string href_string;
     if (node->v.element.tag == GUMBO_TAG_A &&
-        (href = gumbo_get_attribute(&node->v.element.attributes, "href"))) {
-      // std::cout << std::string(href->value) << std::endl;
-      std::string href_string =
-          parse_url(webpage_address, std::string(href->value));
-
-      // size_t pos = href_string.find("://");
-      page_hrefs.push_back(new std::string(href_string));
+        (href = gumbo_get_attribute(&node->v.element.attributes, "href")) &&
+        !(href_string = std::string(href->value)).empty()) {
+      std::string parsed_href_string = parse_url(webpage_address, href_string);
+      if (!parsed_href_string.empty()) {
+        page_hrefs.push_back(new std::string(parsed_href_string));
+      }
     }
 
     GumboVector *children = &node->v.element.children;
@@ -99,27 +99,15 @@ WebPageScraper::parse_html(std::string page_html, std::string webpage_address) {
 
 std::string WebPageScraper::parse_url(std::string base, std::string href) {
   SoupURI *_base = soup_uri_new((base).c_str());
-  if (!SOUP_URI_IS_VALID(_base)) {
-    std::cout << "BAAAAAD: " << base << std::endl;
-  }
+  SoupURI *_url = href.find_first_of(":") > href.find_first_of("/#?")
+                      ? _url = soup_uri_new_with_base(_base, href.c_str())
+                      : _url = soup_uri_new(href.c_str());
 
-  if (href.find_first_of(":") > href.find_first_of("/#?")) {
-    std::cout << "WHAT THE FUCK: " << base << " " << href << std::endl;
-
-    SoupURI *_url = soup_uri_new(("http://" + href).c_str());
-    soup_uri_set_host(_url, soup_uri_get_host(_base));
-    std::cout << std::string(soup_uri_to_string(_url, FALSE)) << std::endl;
-    pthread_exit(NULL);
+  if (!SOUP_URI_VALID_FOR_HTTP(_url)) {
+    return "";
   }
-  SoupURI *_url = href.find_first_of(":") < href.find_first_of("/#?")
-                      ? soup_uri_new(href.c_str())
-                      : soup_uri_new_with_base(_base, href.c_str());
 
   soup_uri_set_fragment(_url, NULL);
-  if (_url == NULL) {
-    std::cout << base << " -> " << href << base << std::endl;
-    // return "";
-  }
-
+  soup_uri_set_query(_url, NULL);
   return std::string(soup_uri_to_string(_url, FALSE));
 }
