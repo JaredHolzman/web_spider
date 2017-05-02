05775acacc6512e73c86982fac4db929deeed6d8
Jared Holzman <jared@holzman.io>
2017-05-02 01:51:14 -0400

    Rerun trial if errors

--- experiments.cc
+++ experiments.cc
@@ -40,34 +40,51 @@ int main(int argc, char *argv[]) {
     output << max_threads;
 
     for (size_t trials = 0; trials < 20; trials++) {
-      std::chrono::time_point<std::chrono::system_clock> curr_time;
-      curr_time = std::chrono::system_clock::now();
-      std::time_t curr_timestamp =
-          std::chrono::system_clock::to_time_t(curr_time);
-        char *time = std::ctime(&curr_timestamp);
-          time[strlen(time) - 2] = '\0';
-      if (trial_count.is_open()) {
-        trial_count << time << " Threads:" << max_threads << " Trial: " << trials << " "
-                    << time << std::endl;
-      }
-      std::cout << time << " Threads:" << max_threads << " Trial: " << trials << " "
-                << std::endl;
-
-      const std::string &curl_log = "./logs/curl_log_" +
-                                    std::to_string(max_threads) + "_" +
-                                    std::to_string(trials);
+      int count = 0;
+      bool no_fail = true;
       double delta = 0.0;
-      std::string root_webpage_address = std::string(argv[1]);
-      ThreadsafeExQueue<Page> ts_queue;
-      HTMLScraper scraper(root_webpage_address, curl_log);
-      WebspiderThreadpools spider(root_webpage_address, max_threads, 4,
-                                  ts_queue, scraper, false);
+      while (count == 0 && !no_fail) {
+        std::chrono::time_point<std::chrono::system_clock> curr_time;
+        curr_time = std::chrono::system_clock::now();
+        std::time_t curr_timestamp =
+            std::chrono::system_clock::to_time_t(curr_time);
+        char *time = std::ctime(&curr_timestamp);
+        time[strlen(time) - 2] = '\0';
+        if (trial_count.is_open()) {
+          trial_count << time << " Threads:" << max_threads
+                      << " Trial: " << trials << " " << time << std::endl;
+        }
+        std::cout << time << " Threads:" << max_threads << " Trial: " << trials
+                  << " " << std::endl;
 
-      double start_time = GetMonotonicTime();
-      spider.crawl_web();
-      double end_time = GetMonotonicTime();
-      delta += end_time - start_time;
+        const std::string &curl_log = "./logs/curl_log_" +
+                                      std::to_string(max_threads) + "_" +
+                                      std::to_string(trials);
+        double delta = 0.0;
+        std::string root_webpage_address = std::string(argv[1]);
+        ThreadsafeExQueue<Page> ts_queue;
+        HTMLScraper scraper(root_webpage_address, curl_log);
+        WebspiderThreadpools spider(root_webpage_address, max_threads, 4,
+                                    ts_queue, scraper, false);
 
+        double start_time = GetMonotonicTime();
+        spider.crawl_web();
+        double end_time = GetMonotonicTime();
+        delta = end_time - start_time;
+
+        std::ifstream trial_log;
+        trial_log.open("./logs/output.txt");
+
+        std::string line;
+        while (getline(trial_log, line)) {
+          no_fail =
+              line.find("curl_easy_perform() failed", 0) != std::string::npos;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        count++;
+      }
       // Write delta time to file
       output << " " << delta;
 
